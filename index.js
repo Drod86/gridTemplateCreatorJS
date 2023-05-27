1568ddec4cfc8c5ddb4d8bdfa6902078af9cd9ae
Drod86 <daniel.r.rodriguez86@gmail.com>
2023-05-27 09:39:42 -0500

    set up initial application functionality JS

---
+++ index.js
@@ -0,0 +1,20 @@
+// DOM helpers
+const grab = (selector) => document.querySelector(selector);
+const grabAll = (selector) => document.querySelectorAll(selector);
+const listen = (node, e, callback) => node.addEventListener(e, callback);
+const render = (node, markUp, add = false) =>
+  add ? (node.innerHTML += markUp) : (node.innerHTML = markUp);
+// HTML helpers
+const htmlEl = (elName, className, attributes = "") =>
+  `<${elName} class="${className}" ${attributes}>${className}</${elName}>`;
+const markUp = (elArr) => elArr.reduce((acc, el) => (acc += el), "");
+// App elements
+const header = htmlEl("header", "header", 'style="border: 1px solid black"');
+const main = htmlEl("main", "main", 'style="border: 1px solid black"');
+const aside = htmlEl("aside", "aside", 'style="border: 1px solid black"');
+const footer = htmlEl("footer", "footer", 'style="border: 1px solid black"');
+// Build App content
+const container = grab(".container"); // Grab initial node
+const elArr = [header, main, aside, footer]; // list app content
+const elements = markUp(elArr); // create markup
+render(container, elements); // Initial app rendering
