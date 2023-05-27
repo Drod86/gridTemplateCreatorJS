254c44b3273ba958ed4fc04671b4438c4beced1a
Drod86 <daniel.r.rodriguez86@gmail.com>
2023-05-27 11:28:02 -0500

    set up inital grid template

--- index.js
+++ index.js
@@ -6,13 +6,31 @@ const render = (node, markUp, add = false) =>
   add ? (node.innerHTML += markUp) : (node.innerHTML = markUp);
 // HTML helpers
 const htmlEl = (elName, className, attributes = "") =>
-  `<${elName} class="${className}" ${attributes}>${className}</${elName}>`;
+  `<${elName} class="${className}" ${attributes}>${
+    className.split(" ").reverse()[0]
+  }</${elName}>`;
 const markUp = (elArr) => elArr.reduce((acc, el) => (acc += el), "");
 // App elements
-const header = htmlEl("header", "header", 'style="border: 1px solid black"');
-const main = htmlEl("main", "main", 'style="border: 1px solid black"');
-const aside = htmlEl("aside", "aside", 'style="border: 1px solid black"');
-const footer = htmlEl("footer", "footer", 'style="border: 1px solid black"');
+const header = htmlEl(
+  "header",
+  "grid-item header",
+  'style="border: 1px solid black"'
+);
+const main = htmlEl(
+  "main",
+  "grid-item main",
+  'style="border: 1px solid black"'
+);
+const aside = htmlEl(
+  "aside",
+  "grid-item aside",
+  'style="border: 1px solid black"'
+);
+const footer = htmlEl(
+  "footer",
+  "grid-item footer",
+  'style="border: 1px solid black"'
+);
 // Build App content
 const container = grab(".container"); // Grab initial node
 const elArr = [header, main, aside, footer]; // list app content
