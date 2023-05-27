b62a68cec53c8c0bdaf0eefd1ae7651120a1d200
Drod86 <daniel.r.rodriguez86@gmail.com>
2023-05-27 18:01:06 -0500

    added menu and example menu functionality

--- index.js
+++ index.js
@@ -11,28 +11,44 @@ const htmlEl = (elName, className, attributes = "") =>
   }</${elName}>`;
 const markUp = (elArr) => elArr.reduce((acc, el) => (acc += el), "");
 // App elements
-const header = htmlEl(
-  "header",
-  "grid-item header",
-  'style="border: 1px solid black"'
+const header = htmlEl("header", "grid-item header");
+const mainMenu = htmlEl("menu", "grid-item main-menu main-menu--left");
+const profileMenu = htmlEl(
+  "menu",
+  "grid-item profile-menu profile-menu--right"
 );
-const main = htmlEl(
-  "main",
-  "grid-item main",
-  'style="border: 1px solid black"'
-);
-const aside = htmlEl(
-  "aside",
-  "grid-item aside",
-  'style="border: 1px solid black"'
-);
-const footer = htmlEl(
-  "footer",
-  "grid-item footer",
-  'style="border: 1px solid black"'
-);
+const main = htmlEl("main", "grid-item main");
+const aside = htmlEl("aside", "grid-item aside");
+const footer = htmlEl("footer", "grid-item footer");
 // Build App content
 const container = grab(".container"); // Grab initial node
-const elArr = [header, main, aside, footer]; // list app content
+const elArr = [header, mainMenu, profileMenu, main, aside, footer]; // list app content
 const elements = markUp(elArr); // create markup
 render(container, elements); // Initial app rendering
+
+// main menu functionality
+const menuState = {
+  "grid-row-end": "-1",
+  "grid-column-end": "3",
+};
+
+const mainMenuNode = grab(".main-menu--left");
+console.log(mainMenuNode);
+listen(mainMenuNode, "click", (e) => {
+  if (!e.target.style["grid-row-end"]) {
+    e.target.style["grid-row-end"] = menuState["grid-row-end"];
+    menuState["grid-row-end"] = "auto";
+    e.target.style["grid-column-end"] = menuState["grid-column-end"];
+    menuState["grid-colum-end"] = "auto";
+  } else if (menuState["grid-row-end"] === "auto") {
+    menuState["grid-row-end"] = e.target.style["grid-row-end"];
+    e.target.style["grid-row-end"] = "auto";
+    menuState["grid-column-end"] = e.target.style["grid-column-end"];
+    e.target.style["grid-column-end"] = "auto";
+  } else {
+    e.target.style["grid-row-end"] = menuState["grid-row-end"];
+    menuState["grid-row-end"] = "auto";
+    e.target.style["grid-column-end"] = menuState["grid-column-end"];
+    menuState["grid-column-end"] = "auto";
+  }
+});
